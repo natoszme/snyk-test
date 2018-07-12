6554e85a8b88ab7fd8f54497e1b5c1e57f25707f
natoszme <natoszmedra@gmail.com>
2018-07-12 01:14:28 -0300

    Evito usar un getter de una variable de instancia, a la que ya se tenia acceso (directamente con this.legajo)

--- src/main/java/ui/viewmodel/HomeViewModel.java
+++ src/main/java/ui/viewmodel/HomeViewModel.java
@@ -24,7 +24,7 @@ public class HomeViewModel {
 	public void ingresarSiPuede(WindowOwner homeView) {
 		RepoEstudiantes repo = RepoEstudiantes.getInstance();
 		try{
-			Estudiante estudiante = repo.obtenerEstudiantePorLegajo(getLegajo());
+			Estudiante estudiante = repo.obtenerEstudiantePorLegajo(legajo);
 			loginEstudiante(homeView, estudiante);
 		}catch(LegajoInexistenteException e) {
 			throw new UserException("Legajo inexistente");
