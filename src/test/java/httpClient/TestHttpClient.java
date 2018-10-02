e7f0487d6741def9887e025916f4f1d30050fe22
natoszme <natoszmedra@gmail.com>
2018-10-02 00:31:56 -0300

    Adapto tests de HttpClient a los cambios del commit anterior

--- src/test/java/httpClient/TestHttpClient.java
+++ src/test/java/httpClient/TestHttpClient.java
@@ -1,6 +1,7 @@
 package httpClient;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.sun.jersey.api.client.ClientResponse;
@@ -9,21 +10,22 @@ import httpClientService.HttpClient;
 
 public class TestHttpClient {
 	
-	public TestHttpClient() {
+	@Before
+	public void before() {
 		HttpClient.getInstance().setToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMTEyMjIzMzMiLCJybmQiOiJ5SXNmZFIwN2lIR3BRRmVjYU9KT2VRPT0ifQ.9pVJGUXhrJPQ-TptNCt971l0h_1dWqWgMrHAWXJchho");
 	}
 	
 	@Test
 	public void alBuscarUnClienteSeObiteneUnaRespuestaConPrimerNombre() {
-		ClientResponse response = HttpClient.getInstance().dameEstudiante();
-		String json = response.getEntity(String.class);
-		Assert.assertTrue(json.contains("first_name"));
+		String jsonResponse = HttpClient.getInstance().dameEstudiante();
+		Assert.assertTrue(jsonResponse.contains("first_name"));
 	}
 	
 	@Test
 	public void elEstudianteDePruebaSeLlamaMatias() {
-		ClientResponse response = HttpClient.getInstance().dameEstudiante();
-		String json = response.getEntity(String.class);
-		Assert.assertTrue(json.contains("Matias"));
+		String jsonResponse = HttpClient.getInstance().dameEstudiante();
+		Assert.assertTrue(jsonResponse.contains("Matias"));
 	}
+	
+	//TODO agregar mas!
 }
