28c71ba2e8d4ec357b8a71ced1b10b48f1655309
natoszme <natoszmedra@gmail.com>
2018-11-05 11:29:04 -0300

    Agrego llamada a DatosDePrueba desde el runner para ver el token que se genera, y tener el estudiante persistido

--- server/src/main/java/runner/Runner.java
+++ server/src/main/java/runner/Runner.java
@@ -1,5 +1,6 @@
 package runner;
 
+import fixture.DatosDePrueba;
 import router.Router;
 import spark.Spark;
 import spark.debug.DebugScreen;
@@ -8,6 +9,8 @@ public class Runner {
 	public static void main(String[] args) {
 		Spark.port(9500);
 		DebugScreen.enableDebugScreen();
+		
+		new DatosDePrueba().configurar();
 		new Router().configure();
 	}
 }
